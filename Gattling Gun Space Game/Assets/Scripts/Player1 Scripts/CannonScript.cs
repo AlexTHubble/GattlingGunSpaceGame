88d48dd1f0830cba98da76618ce8215d39a7d00b
Alexander.Hubble <alexander.hubble@mymail.champlain.edu>
2018-09-19 19:48:54 -0400

    2 player input
    Added rewind, got 2 player input working correctly

---
+++ Gattling Gun Space Game/Assets/Scripts/Player1 Scripts/CannonScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class CannonScript : MonoBehaviour {
+
+    GameObject bulletPrefab = null;
+    public float shootDelay = 0f;
+    bool delayInititated = false;
+    float currentDelay = 0f;
+
+    List<GameObject> spawnPoints = new List<GameObject>();
+    int currentSpawnPoint = 0;
+    Player player;
+
+    // Use this for initialization
+    void Start ()
+    {
+        bulletPrefab = Resources.Load("Prefabs/Bullet") as GameObject;
+        getBulletSpawns();
+        player = ReInput.players.GetPlayer("Player1");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        ShootBullet();
+	}
+
+
+    void ShootBullet()
+    {
+        if (player.GetButtonSinglePressHold("ShootGun"))
+            if (!delayInititated)
+            {
+                Debug.Log("Player2 shoot");
+                Debug.Log("Current spawnpoint: " + currentSpawnPoint);
+                Instantiate(bulletPrefab, spawnPoints[currentSpawnPoint].gameObject.transform.position, spawnPoints[currentSpawnPoint].gameObject.transform.rotation);
+                delayInititated = true;
+                currentDelay = Time.time + shootDelay;
+
+                currentSpawnPoint++;
+                if (currentSpawnPoint > spawnPoints.Count - 1)
+                {
+                    currentSpawnPoint = 0;
+                }
+            }
+
+        if (delayInititated && currentDelay <= Time.time)
+        {
+            delayInititated = false;
+        }
+
+    }
+
+    void getBulletSpawns()
+    {
+        spawnPoints.Add(gameObject.transform.Find("BulletSpawn1").gameObject);
+        spawnPoints.Add(gameObject.transform.Find("BulletSpawn2").gameObject);
+        spawnPoints.Add(gameObject.transform.Find("BulletSpawn3").gameObject);
+        spawnPoints.Add(gameObject.transform.Find("BulletSpawn4").gameObject);
+    }
+}
---
+++ Gattling Gun Space Game/Assets/Scripts/Player2 Scripts/CannonScriptP2.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class CannonScriptP2 : MonoBehaviour {
+
+    GameObject bulletPrefab = null;
+    public float shootDelay = 0f;
+    bool delayInititated = false;
+    float currentDelay = 0f;
+    Player player;
+
+    List<GameObject> spawnPoints = new List<GameObject>();
+    int currentSpawnPoint = 0;
+
+    // Use this for initialization
+    void Start ()
+    {
+        bulletPrefab = Resources.Load("Prefabs/BulletP2") as GameObject;
+        getBulletSpawns();
+        player = ReInput.players.GetPlayer("Player0");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        ShootBullet();
+	}
+
+    void ShootBullet()
+    {
+        if (player.GetButtonSinglePressHold("ShootGun"))
+            if (!delayInititated)
+            {
+                Debug.Log("Player2 shoot");
+                Debug.Log("Current spawnpoint: " + currentSpawnPoint);
+                Instantiate(bulletPrefab, spawnPoints[currentSpawnPoint].gameObject.transform.position, spawnPoints[currentSpawnPoint].gameObject.transform.rotation);
+                delayInititated = true;
+                currentDelay = Time.time + shootDelay;
+
+                currentSpawnPoint++;
+                if (currentSpawnPoint > spawnPoints.Count - 1)
+                {
+                    currentSpawnPoint = 0;
+                }
+            }
+
+        if (delayInititated && currentDelay <= Time.time)
+        {
+            delayInititated = false;
+        }
+
+    }
+
+    void getBulletSpawns()
+    {
+        spawnPoints.Add(gameObject.transform.Find("BulletSpawn1").gameObject);
+        spawnPoints.Add(gameObject.transform.Find("BulletSpawn2").gameObject);
+        spawnPoints.Add(gameObject.transform.Find("BulletSpawn3").gameObject);
+        spawnPoints.Add(gameObject.transform.Find("BulletSpawn4").gameObject);
+    }
+}
